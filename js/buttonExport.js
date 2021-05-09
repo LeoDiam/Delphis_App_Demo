d62db53d384bebe4478893bc81325b7c60256ffb
ChristosBaztekas <christosbaztekas@gmail.com>
2021-05-09 14:27:55 +0300

    buttons in prediction export

---
+++ js/buttonExport.js
@@ -0,0 +1,10 @@
+document.getElementById('exportButton').addEventListener(
+    'click', function () {
+        confirm("Your file will be downloaded soon");
+    }
+);
+document.getElementById('exportButton1').addEventListener(
+    'click', function () {
+        confirm("Your file will be downloaded soon");
+    }
+);
